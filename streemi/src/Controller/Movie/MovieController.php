20fcc1c10b9ad10655e4c9ab142484d5da846462
AITOUALI-Marouane <aitoualimarouane@gmail.com>
2024-10-01 17:01:37 +0200

    Routes movie controller
    Controller movie en ajoutant les routes

---
+++ streemi/src/Controller/Movie/MovieController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Movie;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MovieController extends AbstractController
+{
+    #[Route(path: '/movie', name: 'movie')]
+    public function index()
+    {
+        return $this->render('movie/index.html.twig');
+    }
+    
+    #[Route(path: '/movie/show', name: 'movie_show')]
+    public function show()
+    {
+        return $this->render('movie/show.html.twig');
+    }
+
+    #[Route(path: '/movie/create', name: 'movie_create')]
+    public function create()
+    {
+        return $this->render('movie/create.html.twig');
+    }
+
+    public function edit()
+    {
+        return $this->render('movie/edit.html.twig');
+    }
+
+    public function delete()
+    {
+        return $this->render('movie/delete.html.twig');
+    }
+}
+
+
+?>
